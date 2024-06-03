3aa292a0daf2f774d36e5dd0c503eab5c70abdb0
Luca Venerando Greco <lucag2001@gmail.com>
2024-06-03 15:48:10 +0200

    first try

---
+++ job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=mpi_singularity-job
+#SBATCH --output=mpi_singularity_output.txt
+#SBATCH --error=mpi_singularity_error.txt # Error file
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH --time=00:10:00
+#SBATCH --partition=g100_all_serial
+
+module load singularity
+
+# run the singularity container and map the current directory to /project
+singularity run matrix_multiplication.sif
