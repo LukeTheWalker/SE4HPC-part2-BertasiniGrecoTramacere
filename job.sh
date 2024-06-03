7a19d8ab5430b0606b9b995cc8d68b60effc5dcf
Luca Venerando Greco <lucag2001@gmail.com>
2024-06-03 23:56:46 +0200

    feat: added CI/CD pipeline

---
+++ job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=mpi_singularity-job
+#SBATCH --output=mpi_singularity_output.txt
+#SBATCH --error=mpi_singularity_error.txt # Error file
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH --time=00:10:00
+#SBATCH --partition=g100_all_serial
+
+module load openmpi
+
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+export HWLOC_COMPONENTS=-gl
+export OMPI_MCA_btl=^openib
+
+# run the singularity container and map the current directory to /project
+mpirun -n 2 singularity run --bind /scratch_local:$TMPDIR matrix_multiplication.sif /project/main
