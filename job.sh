bbff27f0ca628620f1bb5afdd7d996837318ae5f
Luca Venerando Greco <lucag2001@gmail.com>
2024-06-03 23:45:37 +0200

    removed stream redirection

--- job.sh
+++ job.sh
@@ -16,4 +16,4 @@ export HWLOC_COMPONENTS=-gl
 export OMPI_MCA_btl=^openib
 
 # run the singularity container and map the current directory to /project
-mpirun -n 2 singularity run --bind /scratch_local:$TMPDIR matrix_multiplication.sif /project/main > output.txt 2> error.txt
+mpirun -n 2 singularity run --bind /scratch_local:$TMPDIR matrix_multiplication.sif /project/main
