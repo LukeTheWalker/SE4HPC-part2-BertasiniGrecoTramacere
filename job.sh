0f8f901e8fd192e6fe43128ac5412e11e04aaa3a
Luca Venerando Greco <lucag2001@gmail.com>
2024-06-03 22:23:32 +0200

    try single node

--- job.sh
+++ job.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=mpi_singularity-job
 #SBATCH --output=mpi_singularity_output.txt
 #SBATCH --error=mpi_singularity_error.txt # Error file
-#SBATCH --ntasks-per-node=1
-#SBATCH --nodes=2
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
 #SBATCH --time=00:10:00
 #SBATCH --partition=g100_all_serial
 
@@ -14,4 +14,4 @@ export HWLOC_COMPONENTS=-gl
 export OMPI_MCA_btl=^openib
 
 # run the singularity container and map the current directory to /project
-srun -N 2 singularity exec --bind /scratch_local:$TMPDIR matrix_multiplication.sif /project/main
+srun -n 2 singularity exec --bind /scratch_local:$TMPDIR matrix_multiplication.sif /project/main
