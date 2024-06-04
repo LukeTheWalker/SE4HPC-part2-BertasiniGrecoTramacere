30ee310036b0f6516a06b3d3e367190343f15b97
Luca Venerando Greco <lucag2001@gmail.com>
2024-06-04 20:22:17 +0200

    tried removing exports

--- job.sh
+++ job.sh
@@ -12,8 +12,5 @@ module load openmpi
 export TMPDIR=$HOME/tmp
 mkdir -p $TMPDIR
 
-export HWLOC_COMPONENTS=-gl
-export OMPI_MCA_btl=^openib
-
 # run the singularity container and map the current directory to /project
 mpirun -n 2 singularity run --bind /scratch_local:$TMPDIR matrix_multiplication.sif /project/main
